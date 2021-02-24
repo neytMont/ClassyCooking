7e3193e18eaffd714b133f2e4895009e973e3ed7
Rifa Safeer Shah <rifashah28@gmail.com>
2021-02-23 16:37:20 -0800

    Update SQL Create Tables.sql

--- SQL Create Tables.sql
+++ SQL Create Tables.sql
@@ -9,8 +9,15 @@ CREATE TABLE Users(
 
 CREATE TABLE Recpies(
   recpieID int PRIMARY KEY,
-  name varchar(50),
-  timeLength int,
+  recipeName varchar(50),
+  totalTime int,
   difficulty int,
+  directions varchar(200),
+  nutritionFacts int,
   ingredients varchar(150) -- replace this with something that can hold objects
 );
+
+INSERT INTO Recipes(recipeID, recipeName, totalTime, difficulty, directions, nutritionFacts, ingredients)
+VALUES (1, 'Brie Cups', 17, 'easy', 'step 1: step 2: ', 136, 'ingredients')
+VALUES (2, 'Spinach and Banana Power Smoothie', 17, 'easy', 'step 1: step 2: ', 136, 'ingredients')
+VALUES (3, 'Eggplant Burgers', 17, 'easy', 'step 1: step 2: ', 136, 'ingredients')

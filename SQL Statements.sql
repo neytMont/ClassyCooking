39bb5c5264bc8fe3cde6180843c41336025a9cf2
Nathaniel Monte de Ramos <59034202+neytMont@users.noreply.github.com>
2021-03-07 21:05:33 -0800

    UC-14
    have to update that when we have a db

---
+++ SQL Statements.sql
@@ -0,0 +1,16 @@
+SELECT * FROM Users;
+SELECT * FROM Recipes;
+
+SELECT * FROM MainIngredient;
+SELECT * FROM AdditionalIngredient;
+
+-- Delete user account CC-42
+DELETE FROM Users WHERE UID=0;
+
+-- Update user recipe CC-38
+UPDATE Users
+SET recipes="[]"
+WHERE UID=0;
+
+--Delete a user recipe CC-14 (Nathaniel)
+DELETE From Recpies WHere recipename = '**USER will have to enter recipe name as a "verification"**';

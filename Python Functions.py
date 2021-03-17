3ac00e91c0def857271de229b10412e59042d692
Saurav <38475004+SauravC99@users.noreply.github.com>
2021-03-17 00:30:35 -0700

    Added hash function skeleton

--- Python Functions.py
+++ Python Functions.py
@@ -3,3 +3,15 @@ db = None
 
 #Check connection somehow
 
+
+#Hash password TODO
+def hash(passwd):
+    #Hash the password
+    return passwd
+
+
+USERS = "Users (UID, fName, lName, age, email, passHash, recipes)"
+RECIPES = "Recipes (recipeID, recipeName, totalTime, difficulty, calories)"
+DIRECTIONS = "Directions (recipeID, directionID, direction)"
+MAININGREDIENT = "MainIngredient (recipeID, mainName, mainCategory, mainType)"
+ADDITIONALINGREDIENT = "AdditionalIngredient (recipeID, additName, additCategory, additType)"

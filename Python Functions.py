e41c8e8f4d55d556a35a1ddeae42c06fd008c2ac
Saurav <38475004+SauravC99@users.noreply.github.com>
2021-03-17 00:31:07 -0700

    Added UC2

--- Python Functions.py
+++ Python Functions.py
@@ -15,3 +15,19 @@ RECIPES = "Recipes (recipeID, recipeName, totalTime, difficulty, calories)"
 DIRECTIONS = "Directions (recipeID, directionID, direction)"
 MAININGREDIENT = "MainIngredient (recipeID, mainName, mainCategory, mainType)"
 ADDITIONALINGREDIENT = "AdditionalIngredient (recipeID, additName, additCategory, additType)"
+
+# UC2 Create user account
+def createUser(data):
+    #data is either a list or tuple
+    fname, lname, age, email, passwd = data
+    UID = getLastUidFromDB() + 1
+    
+    #Hash the users password
+    passwd = hash(passwd)
+    
+    sql = "INSERT INTO " + USERS + "VALUES " + UID + "," + fname + "," + lname + "," + age + "," + email + "," + passwd + "," + "[]"
+    #Run the sql
+    #Check to make sure entry is in db
+    #return true or false based on if it worked??
+    return True
+    
